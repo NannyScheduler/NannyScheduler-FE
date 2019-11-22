470af8eb14b64f4aebda1d1223f7cdb506fe5785
Jasyn Marais <jasynmarais@gmail.com>
2019-11-22 10:03:49 +0200

    Update.

--- nanny-scheduler/src/components/About.js
+++ nanny-scheduler/src/components/About.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  color: palevioletred;
+  font-weight: bold;
+`;
 
 export default function AboutPage() {
   return (
@@ -12,9 +18,9 @@ export default function AboutPage() {
       <p>
         We understand how hard it is to find a good nanny.
       </p>
-      <Link to="/nannies">
+      <StyledLink to="/nannies">
           Nanny List <br/>
-        </Link>
+        </StyledLink>
     </section>
   );
 }
